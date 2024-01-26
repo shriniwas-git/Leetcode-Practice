a23b19b47a7043d09982cf2b8907b30b4c75a3ce
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2024-01-26 21:42:31 +0530

    Time: 36 ms (20.71%) | Memory: 82.1 MB (7.02%) - LeetSync

---
+++ 72-edit-distance/edit-distance.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int func(int i,int j,string s1,string s2,vector<vector<int>> &dp){
+        if(i<0){
+            return j+1;
+        }
+        if(j<0){
+            return i+1;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s1[i]==s2[j]){
+            return dp[i][j] = func(i-1,j-1,s1,s2,dp);
+        }else{
+            return dp[i][j] = min(1+func(i-1,j,s1,s2,dp),min(1+func(i,j-1,s1,s2,dp),1+func(i-1,j-1,s1,s2,dp)));
+        }
+    }
+    int minDistance(string word1, string word2) {
+        int n= word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n,vector<int> (m,-1));
+        return func(n-1,m-1,word1,word2,dp);
+    }
+};
