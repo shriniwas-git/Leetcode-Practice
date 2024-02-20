51f45285ba75bc4996e493c611108c31b99ffcc0
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2024-02-20 16:46:09 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (50.62%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int func(long long mid,vector<int> &nums,int k){
+        int ct=1;
+        int tsum = 0;
+        for(int i=0;i<nums.size();i++){
+            tsum+=nums[i];
+            if(tsum>mid){
+                ct++;
+                tsum = nums[i];
+            }
+        }
+        if(ct>k){
+            return 1;
+        }
+        return 0;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        int low = *max_element(nums.begin(),nums.end());
+        long long sum = 0;
+        for(auto it: nums){
+            sum+=it;
+        }
+        long long high = sum;
+        while(low<=high){
+            long long mid = (low+high)/2;
+            int val = func(mid,nums,k);
+            if(val==1){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return low;
+    }
+};
