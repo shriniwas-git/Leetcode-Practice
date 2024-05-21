3aee58a369cdd737249c1eb99f9542e27222d559
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2024-05-21 11:52:18 +0530

    Time: 56 ms (84.33%) | Memory: 27.1 MB (80.08%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,46 +1,34 @@
-#include<bits/stdc++.h>
-using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
         sort(nums.begin(),nums.end());
+        int n = nums.size();
         vector<vector<int>> ans;
-        for(int i=0;i<nums.size()-2;i++){
+        for(int i=0;i<n-2;i++){
             if(i!=0 && nums[i]==nums[i-1]){
                 continue;
             }
             int left = i+1;
             int right = n-1;
-            int sum = nums[i];
             while(left<right){
-                sum= (nums[i] + nums[left]+nums[right]);
-                if(sum>0){
-                    right--;
-                }else if(sum<0){
-                    left++;
-                }else{
+                if((nums[i]+nums[left]+nums[right])==0){
                     ans.push_back({nums[i],nums[left],nums[right]});
                     left++;
                     right--;
-                    while(left<right && nums[left]==nums[left-1]){
+                while(left<right && nums[left]==nums[left-1]){
                     left++;
                 }
                 while(left<right && nums[right]==nums[right+1]){
                     right--;
                 }
+                }else if((nums[i]+nums[left]+nums[right])>0){
+                    right--;
+                }else{
+                    left++;
                 }
                 
             }
         }
-        // set<vector<int>> st;
-        // for(auto it: ans){
-        //     st.insert(it);
-        // }
-        // vector<vector<int>> ans1;
-        // for(auto it: st){
-        //     ans1.push_back(it);
-        // }
         return ans;
     }
 };
