fb39775d4cc8d66628859fc508cd5358921e2212
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2024-05-28 15:25:10 +0530

    Time: 32 ms (54.21%) | Memory: 21.5 MB (29.92%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    int func(long long mid,vector<int> &piles,int h){
-        long long ct=0;
+    int func(int mid,vector<int>& piles,int h){
+        long long int ct = 0;
         for(int i=0;i<piles.size();i++){
-            long long val = piles[i]/mid;
+            long long int val = piles[i]/mid;
             if(piles[i]%mid!=0){
-                val+=1;
+                val++;
             }
-            ct+=val;
+            ct += val;
         }
-        // cout << ct <<" " << mid << endl;
-        if(ct>h){
-            return 0;
+        if(ct<=h){
+            return 1;
         }
-        return 1;
+        return 0;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        long long low = 1;
-        long long high = *max_element(piles.begin(),piles.end());
-        long long ans =1;
+        int low = 1;
+        int high = *max_element(piles.begin(),piles.end());
+        int ans = -1;
         while(low<=high){
-            long long mid = (low+high)/2;
-            long long val = func(mid,piles,h);
+            int mid = (low+high)/2;
+            int val = func(mid,piles,h);
             if(val==1){
                 ans = mid;
-                high = mid-1;
+                high = mid - 1;
             }else{
-                low = mid+1;
+                low = mid  + 1;
             }
         }
         return ans;
