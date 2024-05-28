2cd9e5304b6ff70d8abf5ce4b32828381eb6ddc1
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2024-05-28 15:40:27 +0530

    Time: 114 ms (53.47%) | Memory: 69 MB (27.77%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
-    int func(long long mid,vector<int> bloomDay,int m,int k){
-        long long ct = 0;
-        long long tp=0;
+    int func(int mid,vector<int>& bloomDay,int m,int k){
+        int ct = 0;
+        int tc = 0;
         for(int i=0;i<bloomDay.size();i++){
-            if(mid>=bloomDay[i]){
-                tp++;
-            }else{
-                tp=0;
-            }
-            if(tp==k){
+            if(tc==k){
+                tc=0;
                 ct++;
-                tp=0;
+            }
+            if(bloomDay[i]<=mid){
+                tc++;
+            }else{
+                tc=0;
             }
         }
+        if(tc==k){
+            ct++;
+        }
+        cout << mid << " " << ct << endl;
         if(ct>=m){
             return 1;
         }
         return 0;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        long long n = bloomDay.size();
-        long long p =(long long)m*(long long)k;
-        if(p>n){
-            return -1;
-        }
-        long long low = 1;
-        long long high = *max_element(bloomDay.begin(),bloomDay.end());
-        long long ans = -1;
+        int low = 1;
+        int high = *max_element(bloomDay.begin(),bloomDay.end());
+        int ans = -1;
         while(low<=high){
-            long long mid = (low+high)/2;
-            long long val = func(mid,bloomDay,m,k);
+            int mid = (low+high)/2;
+            int val = func(mid,bloomDay,m,k);
             if(val==1){
                 ans = mid;
                 high = mid-1;
             }else{
-                low=mid+1;
+                low = mid + 1;
             }
-
         }
         return ans;
     }
