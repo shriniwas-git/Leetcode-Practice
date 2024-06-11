6bb724e66acdb7900d31d1bcd294f0b55f29cedd
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2024-06-11 18:31:43 +0530

    Time: 39 ms (96.75%) | Memory: 30.2 MB (72.24%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n,vector<int> (m,0));
+        queue<pair<int,int>> q;
+        for(int j=0;j<m;j++){
+            if(grid[0][j]==1){
+                vis[0][j] = 1;
+                q.push({0,j});
+            }
+            if(grid[n-1][j]==1){
+                vis[n-1][j] = 1;
+                q.push({n-1,j});
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(grid[i][0]==1){
+                vis[i][0] = 1;
+                q.push({i,0});
+            }
+            if(grid[i][m-1]==1){
+                vis[i][m-1] = 1;
+                q.push({i,m-1});
+            }
+        }
+
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            int delrow[] = {-1,0,1,0};
+            int delcol[] = {0,1,0,-1};
+            for(int i=0;i<4;i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                    vis[nrow][ncol]=1;
+                    q.push({nrow,ncol});
+                }
+            }
+        }
+        int ct=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1 && vis[i][j]==0){
+                    ct++;
+                }
+            }
+        }
+        return ct;
+    }
+};
