3b91b65f528d4a42560378ac5a4398f1aa6cfdcd
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2024-06-12 16:14:00 +0530

    Time: 18 ms (58.57%) | Memory: 16 MB (94.56%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
-    bool func(vector<int> &col,vector<int> adj[],int n,int cur){
-        queue<int> q;
-        q.push(cur);
-        col[cur]=0;
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            for(auto it: adj[node]){
-                if(col[it]==-1){
-                    col[it] = !col[node];
-                    q.push(it);
-                }else if(col[it]==col[node]){
+    bool func(int node,vector<vector<int>> &graph,int color,vector<int> &col){
+        col[node]=color;
+        for(auto it: graph[node]){
+            if(col[it]==-1){
+                if(func(it,graph,!color,col)==false){
                     return false;
                 }
+            }else if(col[it]==col[node]){
+                return false;
             }
         }
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int>adj[n];
-        for(int i=0;i<n;i++){
-            for(auto x:graph[i]){
-                adj[i].push_back(x);
-            }
-        }
+        int n = graph.size();
+        // int m = graph[0].size();
         vector<int> col(n,-1);
         for(int i=0;i<n;i++){
             if(col[i]==-1){
-                if(func(col,adj,n,i)==false){
+                if(func(i,graph,0,col)==false){
                     return false;
                 }
             }
