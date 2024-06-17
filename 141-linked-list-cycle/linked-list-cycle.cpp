db045b52de511c065b6b4ddbb0900c32434072bf
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2024-06-17 08:33:47 +0530

    Time: 7 ms (84.15%) | Memory: 10.5 MB (63.98%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -11,12 +11,9 @@ public:
     bool hasCycle(ListNode *head) {
         ListNode* slow = head;
         ListNode* fast = head;
-        if(head==NULL || head->next==NULL){
-            return false;
-        }
         while(fast!=NULL && fast->next!=NULL){
             slow = slow->next;
-            fast= fast->next->next;
+            fast = fast->next->next;
             if(slow==fast){
                 return true;
             }
