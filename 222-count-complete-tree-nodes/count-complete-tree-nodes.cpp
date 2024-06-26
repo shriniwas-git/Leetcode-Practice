b327fa6300c1876eb8e67018f60967f732cdf708
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2024-06-26 14:16:17 +0530

    Time: 20 ms (79.61%) | Memory: 29 MB (78.92%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -11,32 +11,10 @@
  */
 class Solution {
 public:
-    int lht(TreeNode* node){
-        int h=0;
-        while(node){
-            h++;
-            node=node->left;
-        }
-        return h;
-    }
-    int rht(TreeNode* node){
-        int h=0;
-        while(node){
-            h++;
-            node = node->right;
-        }
-        return h;
-    }
     int countNodes(TreeNode* root) {
         if(root==NULL){
             return 0;
         }
-        int lh = lht(root);
-        int rh = rht(root);
-        if(lh==rh){
-            return (1<<lh) - 1;
-        }
         return 1 + countNodes(root->left) + countNodes(root->right);
     }
-    
 };
