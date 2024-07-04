d0ab9f9a1c818a85958d30f602b827e9c7698755
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2024-07-04 13:00:08 +0530

    Time: 3 ms (98.12%) | Memory: 18.4 MB (70.66%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int ct1=0;
-        int ct2=0;
-        int el1=0,el2=0;
+        int ct1 = 0;
+        int ct2 = 0;
+        int el1 = INT_MIN,el2 = INT_MIN;
         for(int i=0;i<nums.size();i++){
-            if(ct1==0 && nums[i]!=el2){
-                ct1++;
-                el1=nums[i];
-            }else if(ct2==0 && nums[i]!=el1){
-                ct2++;
-                el2 = nums[i];
-            }else if(nums[i]==el1){
+            if(nums[i]==el1){
                 ct1++;
             }else if(nums[i]==el2){
+                ct2++;
+            }else if(ct1==0){
+                el1 = nums[i];
+                ct1++;
+            }else if(ct2==0){
+                el2 = nums[i];
                 ct2++;
             }else{
                 ct1--;
                 ct2--;
             }
         }
+        vector<int> ls; // list of answers
+
+        // Manually check if the stored elements in
+        // el1 and el2 are the majority elements:
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == el1) cnt1++;
+            if (nums[i] == el2) cnt2++;
+        }
         int n = nums.size();
-        int c1=0,c2=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==el1){
-                c1++;
-            }else if(nums[i]==el2){
-                c2++;
-            }
+
+        int mini = int(n / 3) + 1;
+        if (cnt1 >= mini) ls.push_back(el1);
+        if (cnt2 >= mini) ls.push_back(el2);
+        return ls;
+
         }
-        if(c1>n/3 && c2>n/3){
-        return {el1,el2};
-        }else if(c1>n/3){
-            return {el1};
-        }else if(c2>n/3){
-        return {el2};
-        }
-        return {};
-    }
 };
