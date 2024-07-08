4cf3cfb1db26b8f8d8b5668648a227882dd054a7
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2024-07-08 18:48:47 +0530

    Time: 54 ms (89.06%) | Memory: 27.1 MB (79.78%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,27 +4,27 @@ public:
         sort(nums.begin(),nums.end());
         int n = nums.size();
         vector<vector<int>> ans;
-        for(int i=0;i<n-2;i++){
+        for(int i=0;i<n;i++){
             if(i!=0 && nums[i]==nums[i-1]){
                 continue;
             }
-            int left = i+1;
-            int right = n-1;
-            while(left<right){
-                if((nums[i]+nums[left]+nums[right])==0){
-                    ans.push_back({nums[i],nums[left],nums[right]});
-                    left++;
-                    right--;
-                while(left<right && nums[left]==nums[left-1]){
-                    left++;
-                }
-                while(left<right && nums[right]==nums[right+1]){
-                    right--;
-                }
-                }else if((nums[i]+nums[left]+nums[right])>0){
-                    right--;
+            int j = i+1;
+            int k =n-1;
+            while(j<k){
+                if((nums[i]+nums[j]+nums[k])==0){
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                    while(j<n && nums[j]==nums[j-1]){
+                        j++;
+                    }
+                    while(k>0 && nums[k+1]==nums[k]){
+                        k--;
+                    }
+                }else if((nums[i]+nums[j]+nums[k])>0){
+                    k--;
                 }else{
-                    left++;
+                    j++;
                 }
                 
             }
