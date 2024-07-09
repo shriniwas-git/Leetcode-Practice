50e26fe5d0d2abb0eee41009dd1cb1109b6cb091
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2024-07-09 17:05:25 +0530

    Time: 3 ms (96.03%) | Memory: 8.2 MB (80.13%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int left = 0;
+        int right = 0;
+        int n = s.size();
+        vector<int> hash(26,0);
+        int  mrcf= 0; //max_rep_char_freq
+        int ans = 0;
+        while(right<n){
+            hash[s[right]-'A']++;
+            mrcf = max(mrcf,hash[s[right]-'A']);
+            //Distinct char count --> right-left+1 - (mrcf) --> length-mrcf
+            while(right<n && ((right-left+1)-mrcf>k)){
+                hash[s[left]-'A']--;
+                int temp_max = 0;
+                for(int i=0;i<26;i++){
+                    temp_max = max(temp_max,hash[i]);
+                }
+                left++;
+                mrcf = temp_max;
+            }
+            if((right-left+1)-mrcf<=k){
+                ans = max(ans,right-left+1);
+            }
+            right++;
+        }
+        return ans;
+    }
+};
