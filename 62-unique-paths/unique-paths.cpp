0d07fd8f885e42a48733c0f7adfb660cebf859a8
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2024-07-27 20:17:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (6.54%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -16,7 +16,26 @@ public:
 
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1,vector<int> (n+1,-1));
-        return func(m-1,n-1,dp);
+        vector<vector<int>> dp(m+1,vector<int> (n+1,0));
+        // return func(m-1,n-1,dp);
+        // dp[0][0] = 1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if (i == 0 && j == 0) {
+                dp[i][j] = 1;
+                continue; // Skip the rest of the loop and continue with the next iteration.
+                }
+                int left = 0;
+                if(i>=1){
+                    left = dp[i-1][j];
+                }
+                int up = 0;
+                if(j>0){
+                    up = dp[i][j-1];
+                }
+                dp[i][j] = (left + up);
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
