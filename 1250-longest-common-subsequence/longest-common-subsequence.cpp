3f2d25969dcc6510088b3ea5fc574596bd32540a
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2024-07-28 15:08:23 +0530

    Time: 21 ms (71.39%) | Memory: 25.6 MB (72.86%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -15,18 +15,21 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
-        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        // vector<vector<int>> dp(n+1,vector<int> (m+1,0));
         // return func(n-1,m-1,text1,text2,dp);
+        vector<int> prev(m+1,0);
         for(int ind1=1;ind1<=n;ind1++){
+            vector<int> cur(m+1,0);
             for(int ind2=1;ind2<=m;ind2++){
                 if(text1[ind1-1]==text2[ind2-1]){
-                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                    cur[ind2] = 1 + prev[ind2-1];
                     continue;
                 }
 
-                dp[ind1][ind2] = max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+                cur[ind2] = max(prev[ind2],cur[ind2-1]);
             }
+            prev = cur;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
