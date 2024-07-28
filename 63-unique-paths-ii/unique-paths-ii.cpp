10f2f2f5e2e52b078bcd01d72819ff7faaa9ee2f
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2024-07-28 09:50:48 +0530

    Time: 2 ms (60.53%) | Memory: 10.6 MB (13.09%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    int func(int r,int c,vector<vector<int>>& og,vector<vector<int>> &dp){
-        if(r<0 || c<0){
+    int func(int i,int j,vector<vector<int>> &obstacleGrid,vector<vector<int>> &dp){
+        if(i<0 || j<0){
             return 0;
         }
-        
-        if(og[r][c]==1){
+        if(obstacleGrid[i][j]==1){
             return 0;
         }
-        if(r==0 && c==0){
+        if(i==0 && j==0){
             return 1;
         }
         
-        if(dp[r][c]!=-1){
-            return dp[r][c];
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-        int up = func(r-1,c,og,dp);
-        int left = func(r,c-1,og,dp);
-        return dp[r][c] = up+left;
+        int up = func(i-1,j,obstacleGrid,dp);
+        int left = func(i,j-1,obstacleGrid,dp);
+        return dp[i][j] = (up+left);
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
-        vector<vector<int>> dp(n, vector<int> (m,-1));
-        int ans = func(n-1,m-1,obstacleGrid,dp);
-        return ans;
+        vector<vector<int>> dp(n,vector<int> (m,-1));
+        return func(n-1,m-1,obstacleGrid,dp);
     }
 };
