39d11ac399ede999f8246351cbabccf9edfc9e70
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2024-07-28 16:41:20 +0530

    Time: 1795 ms (7.61%) | Memory: 421.9 MB (6.56%) - LeetSync

---
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int func(int ind1,int ind2,string s,string t,vector<vector<int>> &dp){
+        if(ind2<0){
+            return 1;
+        }
+        if(ind1<0){
+            return 0;
+        }
+        if(dp[ind1][ind2]!=-1){
+            return dp[ind1][ind2];
+        }
+        if(s[ind1]==t[ind2]){
+            return dp[ind1][ind2] = (func(ind1-1,ind2-1,s,t,dp) + func(ind1-1,ind2,s,t,dp));
+        }
+        return dp[ind1][ind2] = func(ind1-1,ind2,s,t,dp);
+    }
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<vector<int>> dp(n,vector<int> (m,-1));
+        return func(n-1,m-1,s,t,dp);
+    }
+};
