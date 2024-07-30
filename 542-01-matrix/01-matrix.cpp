c2f1cea4681e3d4e78afd1924924ae452db989a2
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2024-07-30 10:37:04 +0530

    Time: 49 ms (79.35%) | Memory: 35.5 MB (71.62%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-        vector<vector<int>> vis(n, vector<int> (m,0));
-        vector<vector<int>> dis(n, vector<int> (m,1e9));
-        queue<pair<pair<int,int>,int>> q;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<vector<int>> dist(m,vector<int> (n,1e9));
+        queue<pair<int,pair<int,int>>> q;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
-                    q.push({{i,j},0});
-                    vis[i][j] = 1;
-                    dis[i][j] = 0;
+                    q.push({0,{i,j}});
+                    dist[i][j] = 0;
                 }
             }
         }
-        int dr[] = {-1,0,1,0};
-        int dc[] = {0,1,0,-1};
         while(!q.empty()){
-            auto cur = q.front();
+            auto it = q.front();
             q.pop();
-            int r = cur.first.first;
-            int c = cur.first.second;
-            int dist = cur.second;
-            for(int i=0;i<4;i++){
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]==0){
-                    vis[nr][nc] = 1;
-                    q.push({{nr,nc},dist+1});
-                    dis[nr][nc] = dist+1;
+            int curDist = it.first;
+            int curRow = it.second.first;
+            int curCol = it.second.second;
+            int dr[] = {-1,0,1,0};
+            int dc[] = {0,1,0,-1};
+            for(int i=0;i<=3;i++){
+                int nRow = curRow + dr[i];
+                int nCol = curCol + dc[i];
+                if(nRow>=0 && nRow<m && nCol>=0 && nCol<n && dist[nRow][nCol]==1e9){
+                    dist[nRow][nCol] = curDist + 1;
+                    q.push({curDist+1,{nRow,nCol}});
                 }
             }
+            
         }
-        return dis;
+        return dist;
     }
 };
