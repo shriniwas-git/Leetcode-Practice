970e3f152b0e1792ad4ba48c6e0a04511f0b720a
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2026-02-14 16:01:39 +0530

    Time: 3 ms (53.56%) | Memory: 23.5 MB (83.12%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int ct1 = 0;
-        int ct2 = 0;
-        int el1 = INT_MIN,el2 = INT_MIN;
+        int ct1=0,ct2=0;
+        int el1=INT_MIN,el2=INT_MIN;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==el1){
+            if(ct1==0 && nums[i]!=el2){
+                ct1=1;
+                el1 = nums[i];
+            }else if(ct2==0 && nums[i]!=el1){
+                ct2 = 1;
+                el2 = nums[i];
+            }else if(nums[i]==el1){
                 ct1++;
             }else if(nums[i]==el2){
-                ct2++;
-            }else if(ct1==0){
-                el1 = nums[i];
-                ct1++;
-            }else if(ct2==0){
-                el2 = nums[i];
                 ct2++;
             }else{
                 ct1--;
                 ct2--;
             }
-        }
-        vector<int> ls; // list of answers
 
-        // Manually check if the stored elements in
-        // el1 and el2 are the majority elements:
-        int cnt1 = 0;
-        int cnt2 = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == el1) cnt1++;
-            if (nums[i] == el2) cnt2++;
         }
-        int n = nums.size();
-
-        int mini = int(n / 3) + 1;
-        if (cnt1 >= mini) ls.push_back(el1);
-        if (cnt2 >= mini) ls.push_back(el2);
+        vector<int> ls;
+        ct1=0;
+        ct2=0;
+        for(int i=0;i<nums.size();i++){
+            if(el1==nums[i]){
+                ct1++;
+            }
+             if(el2==nums[i]){
+                ct2++;
+            }
+        }
+        int mini = (int)(nums.size()/3) + 1;
+        if(ct1>=mini){
+            ls.push_back(el1);
+        }
+        if(ct2>=mini){
+            ls.push_back(el2);
+        }
         return ls;
-
-        }
+    }
 };
