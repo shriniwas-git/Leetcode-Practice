d282e7836643dbc1f18cf2b66969b6ccf4135fd9
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2026-05-20 22:50:58 +0530

    Time: 53 ms (36.95%) | Memory: 90.9 MB (44.21%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mpp;
+        for(auto it: nums){
+            mpp[it]++;
+            if(mpp[it]>1){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
