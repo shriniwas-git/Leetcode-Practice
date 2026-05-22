2547096391cb4ef5317c495a8171db3cf69394b6
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2026-05-22 12:01:39 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (60.03%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int robHelper(vector<int> &nums,int ind,vector<int> &dp){
-        int n = nums.size();
-        if(ind>=n){
+    int func(int ind,vector<int> &nums,vector<int> &dp){
+        
+        if(ind>=nums.size()){
             return 0;
         }
         if(dp[ind]!=-1){
             return dp[ind];
         }
-        int robHere = nums[ind] + robHelper(nums,ind+2,dp);
-        int doesntRob = robHelper(nums,ind+1,dp);
-        return dp[ind] = max(robHere,doesntRob);
+        int take = nums[ind] + func(ind+2,nums,dp);
+        int nottake = func(ind+1,nums,dp);
+        return dp[ind] = max(take,nottake);
     }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size()+1,-1);
-        return robHelper(nums,0,dp);
+        int n = nums.size();
+        vector<int> dp(n+1,-1);
+        int ans = func(0,nums,dp);
+        return ans;
     }
 };
