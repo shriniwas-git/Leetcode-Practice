cd81732e0fd7381665dd3954922655e480661452
shriniwas-git <116355470+shriniwas-git@users.noreply.github.com>
2026-05-24 15:31:13 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (96.22%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -11,21 +11,10 @@
  */
 class Solution {
 public:
-    bool check(TreeNode* p,TreeNode* q){
-        if(p==NULL && q==NULL){
-            return true;
-        }
-        if((p==NULL && q!=NULL) || (p!=NULL && q==NULL)){
-            return false;
-        }
-        if(p->val!=q->val){
-            return false;
-        }
-        bool lcheck = check(p->left,q->left);
-        bool rcheck = check(p->right,q->right);
-        return lcheck && rcheck;
-    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        return check(p,q);
+        if(p==NULL || q==NULL){
+            return (p==q);
+        }
+        return (p->val==q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
